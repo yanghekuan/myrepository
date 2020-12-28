ae9187bbc1bbc917bd71c4bcd7ee8d89dfd0315e
admin <admin@DESKTOP-3LLQIEM>
2020-12-28 17:26:49 +0800

    update

--- product-view-service-feign/src/main/java/com/how2j/springcloud/ProductViewServiceFeignApplication.java
+++ product-view-service-feign/src/main/java/com/how2j/springcloud/ProductViewServiceFeignApplication.java
@@ -1,21 +1,69 @@
 package com.how2j.springcloud;
 
-import org.springframework.boot.SpringApplication;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import brave.sampler.Sampler;
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.thread.ThreadUtil;
+import cn.hutool.core.util.NetUtil;
+import cn.hutool.core.util.NumberUtil;
 
 @SpringBootApplication
 @EnableEurekaClient //表示这是个 Eureka客户端
 @EnableDiscoveryClient //表示用于发现eureka 注册中心的微服务
 @EnableFeignClients //表示用于使用 Feign 方式
+@EnableCircuitBreaker // 使得它可以把信息共享给监控中心
 public class ProductViewServiceFeignApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ProductViewServiceFeignApplication.class, args);
+		//判断 rabiitMQ 是否启动
+        int rabbitMQPort = 5672;
+        if(NetUtil.isUsableLocalPort(rabbitMQPort)) {
+            System.err.printf("未在端口%d 发现 rabbitMQ服务，请检查rabbitMQ 是否启动", rabbitMQPort );
+            System.exit(1);
+        }      
+        int port = 0;
+        int defaultPort = 8012;
+        Future<Integer> future = ThreadUtil.execAsync(() ->{
+                int p = 0;
+                System.out.println("请于5秒钟内输入端口号, 推荐  8012 、 8013  或者  8014，超过5秒将默认使用"+defaultPort);
+                Scanner scanner = new Scanner(System.in);
+                while(true) {
+                    String strPort = scanner.nextLine();
+                    if(!NumberUtil.isInteger(strPort)) {
+                        System.err.println("只能是数字");
+                        continue;
+                    }
+                    else {
+                        p = Convert.toInt(strPort);
+                        scanner.close();
+                        break;
+                    }
+                }
+                return p;
+        });
+        try{
+            port=future.get(5,TimeUnit.SECONDS);
+        }
+        catch (InterruptedException | ExecutionException | TimeoutException e){
+            port = defaultPort;
+        }
+        if(!NetUtil.isUsableLocalPort(port)) {
+            System.err.printf("端口%d被占用了，无法启动%n", port );
+            System.exit(1);
+        }
+        new SpringApplicationBuilder(ProductViewServiceFeignApplication.class).properties("server.port=" + port).run(args);
     }
     
 	@Bean
